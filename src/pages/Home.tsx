558ce3636b4e55be95c05773b05b182a6dc72432
jonathanoliveirarocha <jonathandeoliveirarocha2002@gmail.com>
2025-07-25 06:26:26 -0300

    Fixing the .bat file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-
+import UserList from '../components/UserList'
 
 
 export default function Home() {
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50">
       <Header />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        
+        <UserList />
       </main>
     </div>
   )
